b69f5905dbbf6518d401d952eecb2c81bf855263
Sandy Carter <bwrsandman@gmail.com>
2019-09-25 12:15:35 +0200

    bgfx: Fix water reflections

--- src/Entities/Registry.cpp
+++ src/Entities/Registry.cpp
@@ -16,11 +16,11 @@ void Registry::DebugCreateEntities(float x, float y, float z)
 {
 }
 
-void Registry::DrawModels(graphics::ShaderManager& shaderManager)
+void Registry::DrawModels(uint8_t viewId, graphics::ShaderManager &shaderManager)
 {
 	graphics::ShaderProgram* objectShader = shaderManager.GetShader("Object");
 
-	_registry.view<Model, Transform>().each([objectShader](Model& model, Transform& transform) {
+	_registry.view<Model, Transform>().each([viewId, objectShader](Model& model, Transform& transform) {
 		glm::mat4 modelMatrix = glm::mat4(1.0f);
 		modelMatrix           = glm::translate(modelMatrix, transform.position);
 		modelMatrix           = glm::rotate(modelMatrix, transform.rotation.x, glm::vec3(1.0f, 0.0f, 0.0f));
@@ -34,10 +34,10 @@ void Registry::DrawModels(graphics::ShaderManager& shaderManager)
 		const L3DMesh& mesh = Game::instance()->GetMeshPack().GetMesh(static_cast<uint32_t>(model.meshId));
 
 		for (auto& submeshId : model.submeshIds)
-			mesh.Draw(*objectShader, submeshId);
+			mesh.Draw(viewId, *objectShader, submeshId);
 	});
 
-	_registry.view<AnimatedStatic, Transform>().each([objectShader](AnimatedStatic& animated, Transform& transform) {
+	_registry.view<AnimatedStatic, Transform>().each([viewId, objectShader](AnimatedStatic& animated, Transform& transform) {
 		glm::mat4 modelMatrix = glm::mat4(1.0f);
 		modelMatrix           = glm::translate(modelMatrix, transform.position);
 		modelMatrix           = glm::rotate(modelMatrix, transform.rotation.x, glm::vec3(1.0f, 0.0f, 0.0f));
@@ -53,26 +53,26 @@ void Registry::DrawModels(graphics::ShaderManager& shaderManager)
 		if (animated.type == "Norse Gate")
 		{
 			const L3DMesh& mesh = Game::instance()->GetMeshPack().GetMesh((int)MeshId::BuildingNorseGate);
-			mesh.Draw(*objectShader, 0);
+			mesh.Draw(viewId, *objectShader, 0);
 		}
 		else if (animated.type == "Gate Stone Plinth")
 		{
 			const L3DMesh& mesh = Game::instance()->GetMeshPack().GetMesh((int)MeshId::ObjectGateTotemPlinthe);
-			mesh.Draw(*objectShader, 0);
+			mesh.Draw(viewId, *objectShader, 0);
 		}
 		else if (animated.type == "Piper Cave Entrance")
 		{
 			const L3DMesh& mesh = Game::instance()->GetMeshPack().GetMesh((int)MeshId::BuildingMineEntrance);
-			mesh.Draw(*objectShader, 0);
+			mesh.Draw(viewId, *objectShader, 0);
 		}
 		else
 		{
 			const L3DMesh& mesh = Game::instance()->GetMeshPack().GetMesh(0);
-			mesh.Draw(*objectShader, 0);
+			mesh.Draw(viewId, *objectShader, 0);
 		}
 	});
 
-	_registry.view<MobileStatic, Transform>().each([objectShader](MobileStatic& mobile, Transform& transform) {
+	_registry.view<MobileStatic, Transform>().each([viewId, objectShader](MobileStatic& mobile, Transform& transform) {
 		glm::mat4 modelMatrix = glm::mat4(1.0f);
 		modelMatrix           = glm::translate(modelMatrix, transform.position);
 		modelMatrix           = glm::rotate(modelMatrix, transform.rotation.x, glm::vec3(1.0f, 0.0f, 0.0f));
@@ -86,113 +86,113 @@ void Registry::DrawModels(graphics::ShaderManager& shaderManager)
 		// temporary-ish until we read info.dat:
 		MeshId meshID = MeshId::Dummy;
 		if (mobile.type == 0)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::BuildingCelticFenceShort).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::BuildingCelticFenceShort).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 1)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::BuildingCelticFenceTall).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::BuildingCelticFenceTall).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 2)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::Z_SpellRock01).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::Z_SpellRock01).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 3)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::Dummy).Draw(*objectShader, 0);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::Dummy).Draw(viewId, *objectShader, 0);
 		else if (mobile.type == 4)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::SpellSolidShield).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::SpellSolidShield).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 5)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::SingingStone).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::SingingStone).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 6)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::SingingStoneCenter).Draw(*objectShader, 0);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::SingingStoneCenter).Draw(viewId, *objectShader, 0);
 		else if (mobile.type == 7)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::SingingStoneCenter).Draw(*objectShader, 0);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::SingingStoneCenter).Draw(viewId, *objectShader, 0);
 		else if (mobile.type == 8)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::BuildingCampfire).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::BuildingCampfire).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 9)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::Dummy).Draw(*objectShader, 0);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::Dummy).Draw(viewId, *objectShader, 0);
 		else if (mobile.type == 10)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::FeatureIdol).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::FeatureIdol).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 11)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::FeatureIdol).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::FeatureIdol).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 12)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::Dummy).Draw(*objectShader, 0);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::Dummy).Draw(viewId, *objectShader, 0);
 		else if (mobile.type == 13)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::Dummy).Draw(*objectShader, 0);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::Dummy).Draw(viewId, *objectShader, 0);
 		else if (mobile.type == 14)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::BuildingAztecOlmecHead).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::BuildingAztecOlmecHead).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 15)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::BuildingAztecOlmecHead).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::BuildingAztecOlmecHead).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 16)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::Boulder1Chalk).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::Boulder1Chalk).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 17)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::Boulder1Lime).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::Boulder1Lime).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 18)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::Boulder1Sand).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::Boulder1Sand).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 19)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::Boulder1Volcanic).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::Boulder1Volcanic).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 20)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::Boulder2Chalk).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::Boulder2Chalk).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 21)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::Boulder2Lime).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::Boulder2Lime).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 22)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::Boulder2Sand).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::Boulder2Sand).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 23)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::Boulder2Volcanic).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::Boulder2Volcanic).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 24)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::Boulder3Chalk).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::Boulder3Chalk).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 25)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::Boulder3Lime).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::Boulder3Lime).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 26)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::Boulder3Sand).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::Boulder3Sand).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 27)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::Boulder3Volcanic).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::Boulder3Volcanic).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 28)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::ObjectFlatRockChalk).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::ObjectFlatRockChalk).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 29)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::ObjectFlatRockLimeStone).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::ObjectFlatRockLimeStone).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 30)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::ObjectFlatRockSandStone).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::ObjectFlatRockSandStone).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 31)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::ObjectFlatRockVolcanic).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::ObjectFlatRockVolcanic).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 32)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::ObjectLongRockChalk).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::ObjectLongRockChalk).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 33)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::ObjectLongRockLimeStone).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::ObjectLongRockLimeStone).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 34)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::ObjectLongRockSandStone).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::ObjectLongRockSandStone).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 35)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::ObjectLongRockVolcanic).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::ObjectLongRockVolcanic).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 36)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::ObjectRockChalk).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::ObjectRockChalk).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 37)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::ObjectRockLimeStone).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::ObjectRockLimeStone).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 38)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::ObjectRockSandStone).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::ObjectRockSandStone).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 39)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::ObjectRockVolcanic).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::ObjectRockVolcanic).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 40)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::ObjectSharpRockChalk).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::ObjectSharpRockChalk).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 41)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::ObjectSharpRockLimeStone).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::ObjectSharpRockLimeStone).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 42)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::ObjectSharpRockSandStone).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::ObjectSharpRockSandStone).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 43)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::ObjectSharpRockVolcanic).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::ObjectSharpRockVolcanic).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 44)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::ObjectSquareRockChalk).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::ObjectSquareRockChalk).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 45)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::ObjectSquareRockLimeStone).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::ObjectSquareRockLimeStone).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 46)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::ObjectSquareRockSandStone).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::ObjectSquareRockSandStone).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 47)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::ObjectSquareRockVolcanic).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::ObjectSquareRockVolcanic).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 48)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::ObjectFlatRockVolcanic).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::ObjectFlatRockVolcanic).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 49)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::ObjectGateTotemApe).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::ObjectGateTotemApe).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 50)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::ObjectGateTotemBlank).Draw(*objectShader, 0); // 1 is the phys mesh.... ???
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::ObjectGateTotemBlank).Draw(0, *objectShader, 0); // 1 iviewId the phys mesh.... ???
 		else if (mobile.type == 51)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::ObjectGateTotemCow).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::ObjectGateTotemCow).Draw(viewId, *objectShader, 1);
 		else if (mobile.type == 52)
-			Game::instance()->GetMeshPack().GetMesh((int)MeshId::ObjectGateTotemTiger).Draw(*objectShader, 1);
+			Game::instance()->GetMeshPack().GetMesh((int) MeshId::ObjectGateTotemTiger).Draw(viewId, *objectShader, 1);
 		else
-			Game::instance()->GetMeshPack().GetMesh(0).Draw(*objectShader, 0);
+			Game::instance()->GetMeshPack().GetMesh(0).Draw(viewId, *objectShader, 0);
 	});
 
 	//
