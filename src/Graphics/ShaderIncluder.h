39615c44266c375658fdd2d51e8fd472cc1c5962
Sandy Carter <bwrsandman@gmail.com>
2025-01-26 15:48:13 -0500

    shader includer: Update spirv path for new bgfx version

--- src/Graphics/ShaderIncluder.h
+++ src/Graphics/ShaderIncluder.h
@@ -33,7 +33,7 @@
 
 // Concatenate the four elements of your path.
 #include CONCAT5(GENERATED_SHADERS_DIR, SHADER_DIR,glsl/, SHADER_NAME,.sc.bin.h)
-#include CONCAT5(GENERATED_SHADERS_DIR, SHADER_DIR,spv/, SHADER_NAME,.sc.bin.h)
+#include CONCAT5(GENERATED_SHADERS_DIR, SHADER_DIR,spirv/, SHADER_NAME,.sc.bin.h)
 #include CONCAT5(GENERATED_SHADERS_DIR, SHADER_DIR,essl/, SHADER_NAME,.sc.bin.h)
 #if defined(_WIN32)
 #include CONCAT5(GENERATED_SHADERS_DIR, SHADER_DIR,dx11/, SHADER_NAME,.sc.bin.h)
